0e9418a5b484866cefdc8e99fce348e4a245d2fd
albinap <albinap952654@gmail.com>
2026-01-26 22:31:00 +0530

    signup added with api

--- app/src/main/java/com/example/healthcare/api/RetrofitClient.kt
+++ app/src/main/java/com/example/healthcare/api/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package com.example.healthcare.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -7,7 +9,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "https://<eldercare-api>/"
+    private const val BASE_URL =
+        "https://disproportionable-unantagonistic-alvin.ngrok-free.dev/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -16,12 +19,15 @@ object RetrofitClient {
     private val client = OkHttpClient.Builder()
         .addInterceptor(logging)
         .build()
+    private val gson: Gson = GsonBuilder()
+        .setLenient()
+        .create()
 
     val api: ElderApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ElderApiService::class.java)
     }
